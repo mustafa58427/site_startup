cd3e119368a1c15959e66a6368871db9019c0f91
Mustafa <mustafa58427@gmail.com>
2022-05-10 15:37:03 +0100

    original files and folders

---
+++ includes/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+	
+<html lang="en">
+	
+	<head>
+		<title>Mustafa Mohammed</title>
+		<meta charset="utf-8">		
+		<meta name="viewport" content="width=device-width, initial-scale=1">				
+		
+		<link rel="stylesheet" type="text/css" href="css/public.css">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+		
+		
+		<link href='http://fonts.googleapis.com/css?family=Lato:400,700,900,300' rel='stylesheet' type='text/css'>
+		<link href="http://fonts.googleapis.com/css?family=Open+Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic" rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Raleway:400,300,600,700,900' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+	</head>
+	
+	<body>
+		<div id="wrapper">
+		
+			<header class="container">
+				<div class="jumbotron jumbotron-fluid" id="banner">
+					<h2 class="display-2">Bootstrap 4 alpha is here</h2>
+					<h3 class="learn">Wanna know how to use it</h3>
+				</div>
+			</header>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include("includes/header.php"); ?>
+			
+    <div class="container-fluid">
+        <h1>PHP5 Tests</h1>
+    
+        
+        <div class="row">
+            <div class="col-sm-4" style="background-color:lavender;">.col-sm-4</div>
+            <div class="col-sm-4" style="background-color:lavenderblush;">.col-sm-4</div>
+            <div class="col-sm-4" style="background-color:lavender;">.col-sm-4</div>
+        </div>
+    </div>
+    
+<?php include("includes/footer.php"); ?>	
