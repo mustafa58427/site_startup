f4267ad6d1cb4149030c4755ebf63b485f3bc867
Mustafa <mustafa58427@gmail.com>
2022-05-10 16:11:14 +0100

    removed unecessary header

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
         </div><!-- end of wrapper -->
 		
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>		
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 		<script type="text/javascript" src="public/js/bootstrap.min.js"></script>
 		<script type="text/javascript" src="public/js/bootstrap.js"></script>
 		<script type="text/javascript" src="public/js/npm.js"></script>
--- includes/header.php
+++ includes/header.php
@@ -9,8 +9,7 @@
 		
 		<link rel="stylesheet" type="text/css" href="public/css/public.css">
 		<link rel="stylesheet" type="text/css" href="public/css/bootstrap.min.css">
-		<link rel="stylesheet" type="text/css" href="public/css/bootstrap.css">
-		
+		<link rel="stylesheet" type="text/css" href="public/css/bootstrap.css">		
 		
 		<link href='http://fonts.googleapis.com/css?family=Lato:400,700,900,300' rel='stylesheet' type='text/css'>
 		<link href="http://fonts.googleapis.com/css?family=Open+Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic" rel='stylesheet' type='text/css'>
@@ -24,7 +23,6 @@
 		
 			<header class="container">
 				<div class="jumbotron jumbotron-fluid" id="banner">
-					<h2 class="display-2">Bootstrap 4 alpha is here</h2>
-					<h3 class="learn">Wanna know how to use it</h3>
+					<h2 class="display-2">Bootstrap 4 alpha is here</h2>		
 				</div>
 			</header>
